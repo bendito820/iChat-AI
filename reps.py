1a9a24cc6822ab8614feadbbf221eb5a5bb435e5
Antonio Costa <bendito820@gmail.com>
2024-04-16 16:18:39 +0100

    first rep, in the reps file

--- reps.py
+++ reps.py
@@ -4,7 +4,7 @@ from groq import Groq
 
 
 client = Groq(
-    api_key=''
+    api_key="my_key"
 )
 
 r = sr.Recognizer()
@@ -12,50 +12,51 @@ r = sr.Recognizer()
 
 def change_voice(engine, language, gender="None"):
     for voice in engine.getProperty('voices'):
-        engine.setProperty('voice')
-        return True
+        if language.lower() in voice.name.lower():
+            engine.setProperty('voice', voice.id)
+            return True
 
     raise RuntimeError(
-        "LAnguage '{}' for gender '{}' not found".format(language, gender)
+        ""
     )
 
 
-def SpeakText(command):
+def SpeakText():
     engine = pyttsx3.init()
     change_voice(engine, 'portuguese')
     engine.say(command)
     engine.runAndWait()
 
 
-SpeakText('E la vamos mais uma vez')
-
-
 while True:
     try:
-        print('you can start speaking...')
+        print('You can start speaking...')
         with sr.Microphone() as source2:
             r.adjust_for_ambient_noise(source2, duration=0.2)
+
             audio2 = r.listen(source2)
 
-            MyText = r.recognize_google_cloud(audio2, language='en')
+            MyText = r.recognize_google_cloud(audio2, language='pt-BR')
             MyText = MyText.lower()
 
             if MyText == 'exit':
                 break
 
             print(f'Prompt: {MyText}')
-            chat_completion = client.chat.completions.create(messages=[{
-                'role': 'user',
-                'content': MyText
-            }], model='mixtral')
-            print('Answer: ', chat_completion.choices[0].message.content)
+            chat_completion = client.chat.completions.create(
+                messages=[
+                    {
+                        'role': 'user',
+                        'content': f' {MyText}'
+                    }
+                ],
+                model='mixtral-8x7b-32768'
+            )
+
+            print(f'Answer: {chat_completion.choices[0].message.content}')
             SpeakText(chat_completion.choices[0].message.content)
     except sr.RequestError as e:
-        print('Could not request results; {0}'.format(e))
+        print(e)
 
     except sr.UnknownValueError:
-        print('unknown error occurred')
-
-engine = pyttsx3.init()
-for voice in engine.getProperty('voices'):
-    print(voice)
+        print('unknoen error occurred')
